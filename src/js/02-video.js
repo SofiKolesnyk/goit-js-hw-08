5c8d2ef7cdde89a22b5d3d2b171929d6d655cb14
SofiKolesnyk <sofi_serg@ukr.net>
2022-10-31 19:45:39 +0200

    hw video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import  throttle from"lodash.throttle"; 
 
+const iframe = document.querySelector('iframe');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(event) {
+     
+    localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
+};
+
+const getLocalStorageValue = localStorage.getItem(LOCALSTORAGE_KEY); 
+
+if (getLocalStorageValue === null || getLocalStorageValue === undefined) {
+    return ;
+} else {
+    player.setCurrentTime(getLocalStorageValue);
+};
+
+   
